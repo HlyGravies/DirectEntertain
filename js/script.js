b221d90a5dbe411895b08966862897dbafcd3f7a
HlyGravies <nguyenhoang887984@gmail.com>
2024-10-22 16:38:02 +0900

    Sửa lại hiển thị mật khẩu (không thành công)

--- js/script.js
+++ js/script.js
@@ -1,43 +1,58 @@
 // Guarantee JavaScript only run after DOM structure finished loading
 document.addEventListener("DOMContentLoaded", function () {
-  // To show password for login and register page
-  var passwordInput = document.getElementById("password");
-  var togglePasswordBtn = document.getElementById("togglePassword");
+    // To show password for login and register page
+    let passwordInput = document.getElementById("password");
+    let togglePasswordBtn = document.getElementById("togglePassword");
 
-  if (togglePasswordBtn) {
-    togglePasswordBtn.addEventListener("click", function () {
-      if (passwordInput.type === "password") {
-        passwordInput.type = "text";
-        togglePasswordBtn.textContent = "非表示";
-      } else {
-        passwordInput.type = "password";
-        togglePasswordBtn.textContent = "表示";
-      }
-    });
-  }
+    let registerPasswordInput = document.getElementById("rePassword");
+    let toggleRePasswordBtn = document.getElementById("toggleRePassword");
 
-  // To show confirm password content (Only in register page)
-  var confirmPasswordInput = document.getElementById("confirm_password");
-  var toggleConfirmPasswordBtn = document.getElementById("toggleConfirmPassword");
+    let confirmPasswordInput = document.getElementById("confirm_password");
+    let toggleConfirmPasswordBtn = document.getElementById("toggleConfirmPassword");
 
-  if (confirmPasswordInput && toggleConfirmPasswordBtn) {
-    toggleConfirmPasswordBtn.addEventListener("click", function () {
-      if (confirmPasswordInput.type === "password") {
-        confirmPasswordInput.type = "text";
-        toggleConfirmPasswordBtn.textContent = "非表示";
-      } else {
-        confirmPasswordInput.type = "password";
-        toggleConfirmPasswordBtn.textContent = "表示";
-      }
-    });
+    if (togglePasswordBtn) {
+        togglePasswordBtn.addEventListener("click", function () {
+            if (passwordInput.type === "password") {
+                passwordInput.type = "text";
+                togglePasswordBtn.textContent = "非表示";
+            } else {
+                passwordInput.type = "password";
+                togglePasswordBtn.textContent = "表示";
+            }
+        });
+    }
 
-    // Confirm password before register form being sent
-    document.getElementById("registrationi-form").addEventListener("submit", function (event) {
-      if (passwordInput.value !== confirmPasswordInput) {
-        document.getElementById("response").innerText = "Password incorrect";
-        document.getElementById("repsonse").style.color = "red";
-        event.preventDefault();
-      }
-    });
-  }
+    if (registerPasswordInput && toggleRePasswordBtn) {
+        toggleRePasswordBtn.addEventListener("click", function () {
+            if (registerPasswordInput.type === "password") {
+                registerPasswordInput.type = "text";
+                togglePasswordBtn.textContent = "非表示";
+            } else {
+                registerPasswordInput.type = "password";
+                togglePasswordBtn.textContent = "表示";
+            }
+        });
+    }
+
+    // To show confirm password content (Only in register page)
+    // if (confirmPasswordInput && toggleConfirmPasswordBtn) {
+    //     toggleConfirmPasswordBtn.addEventListener("click", function () {
+    //         if (confirmPasswordInput.type === "password") {
+    //             confirmPasswordInput.type = "text";
+    //             toggleConfirmPasswordBtn.textContent = "非表示";
+    //         } else {
+    //             confirmPasswordInput.type = "password";
+    //             toggleConfirmPasswordBtn.textContent = "表示";
+    //         }
+    //     });
+    //
+    //     // Confirm password before register form being sent
+    //     document.getElementById("registration-form").addEventListener("submit", function (event) {
+    //         if (registerPassword.value !== confirmPasswordInput) {
+    //             document.getElementById("response").innerText = "Password incorrect";
+    //             document.getElementById("response").style.color = "red";
+    //             event.preventDefault();
+    //         }
+    //     });
+    // }
 });
