08026af32c308d856697e0f85315fe6ffe505257
HlyGravies <nguyenhoang887984@gmail.com>
2024-10-29 00:48:12 +0900

    Done 29/10

--- API/function.php
+++ API/function.php
@@ -1,21 +1,48 @@
 <?php
-// kiểm tra xem người dùng có tồn tại hay không
-function isUserIdExist($pdo, $userId){
+// kiểm tra xem ID người dùng có tồn tại hay không
+function isUserIdExist($pdo, $userId)
+{
     $getUserSql = "SELECT userId FROM users WHERE userId = :userId";
-    $getUserStmt = $pdo -> prepare($getUserSql);
-    $getUserStmt -> bindParam(':userId', $userId);
-    $getUserStmt -> execute();
-    return $getUserStmt -> fetch(PDO::FETCH_ASSOC);
-}
-
-// Lấy thông tin của user
-function getUserInfo($pdo, $userId) {
-    $getUserSql = "SELECT userId, userName, profile, iconPath FROM users WHERE userId = :userId";
     $getUserStmt = $pdo->prepare($getUserSql);
     $getUserStmt->bindParam(':userId', $userId);
     $getUserStmt->execute();
     return $getUserStmt->fetch(PDO::FETCH_ASSOC);
 }
+
+// Kiểm tra xem tên của nguười đùng có tồn tại hay không
+function isUserNameExist($pdo, $username)
+{
+    $getUserSql = "SELECT username FROM users WHERE username = :username";
+    $getUserStmt = $pdo->prepare($getUserSql);
+    $getUserStmt->bindParam(':username', $username);
+    $getUserStmt->execute();
+    return $getUserStmt->fetch(PDO::FETCH_ASSOC);
+}
+
+function userAuthentication($pdo, $loginData)
+{
+    $getUserSql = "SELECT password FROM users WHERE username = :username";
+    $getUserStmt = $pdo->prepare($getUserSql);
+    $getUserStmt->bindParam(':username', $loginData["username"]);
+    $getUserStmt->execute();
+    $password = $getUserStmt->fetchColumn();
+    if ($loginData["password"] === $password) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Lấy thông tin của user
+function getUserInfo($pdo, $username)
+{
+    $getUserSql = "SELECT userId, userName, email, iconPath FROM users WHERE username = :username";
+    $getUserStmt = $pdo->prepare($getUserSql);
+    $getUserStmt->bindParam(':username', $username);
+    $getUserStmt->execute();
+    return $getUserStmt->fetch(PDO::FETCH_ASSOC);
+}
+
 
 
 ?>
