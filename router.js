c4bacc15d97b72b51b492dfab07530ff9ad643e1
HlyGravies <nguyenhoang887984@gmail.com>
2024-11-06 20:21:33 +0900

    For future develop

---
+++ router.js
@@ -0,0 +1,22 @@
+// // router.js
+// import {createRouter, createWebHashHistory, createWebHistory} from 'vue-router';
+// import MyDec from './Front/FutureDev/myDec.vue';
+// import VideoDec from './Front/FutureDev/videoDec.vue';
+// import MusicDec from './Front/FutureDev/musicDec.vue';
+// import PictureDec from './Front/FutureDev/pictureDec.vue';
+// import SubDec from './Front/FutureDev/subDec.vue';
+//
+// const routes = [
+//     { path: '/', name: 'MyDec', component: MyDec },  // Sửa thành path "/"
+//     { path: '/video-dec', name: 'VideoDec', component: VideoDec },
+//     { path: '/music-dec', name: 'MusicDec', component: MusicDec },
+//     { path: '/picture-dec', name: 'PictureDec', component: PictureDec },
+//     { path: '/favorite-dec', name: 'FavoriteDec', component: SubDec }
+// ];
+//
+// const router = createRouter({
+//     history: createWebHashHistory(),
+//     routes
+// });
+//
+// export default router;
